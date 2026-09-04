c28a118ccb3c6d069d2175df5e94212ab64333fe
haochenzhang <haochen.zhang.1@student.unimelb.edu.au>
2026-09-04 19:28:33 +1000

    Implement items API for volunteer request acceptance; US 11

--- backend/src/items.ts
+++ backend/src/items.ts
@@ -16,59 +16,61 @@ const supabase = createClient(
 // Volunteer requesting list of filtered items
 // US 10
 
-app.get("/items", async (request, reply) => {
-  const authHeader = request.headers.authorization;
+export default async function itemAPI(app: FastifyInstance) {
 
-  // check auth header format, return error when invalid
-  if (!authHeader?.startsWith("Bearer ")) {
-    return reply.status(401).send({
-      error: "Missing or invalid authorisation header"
+    app.get("/items", async (request, reply) => {
+    const authHeader = request.headers.authorization;
+
+    // check auth header format, return error when invalid
+    if (!authHeader?.startsWith("Bearer ")) {
+        return reply.status(401).send({
+        error: "Missing or invalid authorisation header"
+        });
+    }
+
+    const token = authHeader.substring("Bearer ".length);
+
+    // get current user, check for authorisation
+    const {
+        data: { user },
+        error: authError
+    } = await supabase.auth.getUser(token);
+
+    if (authError || !user) {
+        return reply.status(401).send({
+        error: "Unauthorised"
+        });
+    }
+
+    const supabaseUser = createClient(
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_PUBLISH_KEY!,
+        {
+            accessToken: async() => token
+        }
+        )
+
+    // get item list
+    const { data, error } = await supabaseUser
+        .from("items")
+        .select("*");
+
+
+    if (error) {
+        return reply.status(500).send({
+        error: error.message
+        });
+    }
+
+    return reply.send(data);
     });
-  }
-
-  const token = authHeader.substring("Bearer ".length);
-
-  // get current user, check for authorisation
-  const {
-    data: { user },
-    error: authError
-  } = await supabase.auth.getUser(token);
-
-  if (authError || !user) {
-    return reply.status(401).send({
-      error: "Unauthorised"
-    });
-  }
-
-  const supabaseUser = createClient(
-      process.env.SUPABASE_URL!,
-      process.env.SUPABASE_PUBLISH_KEY!,
-      {
-        accessToken: async() => token
-      }
-    )
-
-  // get item list
-  const { data, error } = await supabaseUser
-    .from("items")
-    .select("*");
-
-
-  if (error) {
-    return reply.status(500).send({
-      error: error.message
-    });
-  }
-
-  return reply.send(data);
-});
 
 
 // POST Accept
 // Volunteer accepts a pending item/request
 // US 11
 
-export default async function itemAPI(app: FastifyInstance) {
+
   app.post("/items/:id/accept", async (request, reply) => {
     const authHeader = request.headers.authorization;
 
@@ -159,8 +161,9 @@ export default async function itemAPI(app: FastifyInstance) {
         .send({ error: "Pickup created but failed to update item status" });
     }
 
-    
+        
     // return pickup
     return reply.status(201).send(pickup);
-  });
+    
+    });
 }
